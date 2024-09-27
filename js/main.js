7b759f3fb2c5a6565f0adc9666c4f754c9691298
Dmitry <111519715+4SV-idark@users.noreply.github.com>
2024-09-27 18:35:06 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/* Скрипт для slick Начало */
+$('.autoplay').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    rtl: true,
+    variableWidth: true,
+    centerMode: true,
+});
+
+$('.autoplay-left').slick({
+    rtl: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    variableWidth: true,
+    centerMode: true,
+});
+/* Скрипт для slick Конец */
